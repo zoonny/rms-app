c17f0fcd3f1e8fa544ad88c8b3c841ddae7dc034
zoonny <hyungii@naver.com>
2024-12-25 20:17:41 +0900

    update

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,15 +2,17 @@ from sqlalchemy import create_engine, Column, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-import logging
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
-
 # DATABASE_URL = "postgresql://stgbsuser:sideV1234!@10.97.226.87:5432/stgbsdb"
 # DATABASE_URL = "postgresql://stgbsuser:sideV1234!@52.231.105.193:31432/stgbsdb"
 DATABASE_URL = "postgresql://stgbsuser:sideV1234!@host.docker.internal:5432/stgbsdb"
 
 engine = create_engine(DATABASE_URL)
+
+# db logging
+import logging
+logging.basicConfig()
+logging.getLogger("sqlalchemy.engine").setLevel(logging.ERROR)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
--- backend/tests/query_project_member.py
+++ backend/tests/query_project_member.py
@@ -6,10 +6,23 @@ from models import Member, Project, Team, User, WeeklyReport, ProjectMember
 from datetime import datetime, date
 
 def query(session: Session):
-    project: Project = session.query(Project).filter(Project.name == '하나은행').first()
-    print(vars(project))
-    print(len(project.member_links))
-    for member_link in project.member_links:
-        print(member_link.member.name)
+    team: Team = session.query(Team).filter(Team.name == '전략개발팀').first()
+    pprint(team)
+    for member in team.members:
+        # print(f'사번:{member.id}, 성명:{member.name}')
+        for project_link in member.project_links:
+            project: Project = project_link.project
+            print(f'사번:{member.id}, 성명:{member.name}, 프로젝트:{project.name}')
+
+            for member_link in project.member_links:
+                project_member: Member = member_link.member
+                if member.id != project_member.id:
+                    print(f'협업멤버 - 사번:{project_member.id}, 성명:{project_member.name}')
+
+    # project: Project = session.query(Project).filter(Project.name == '하나은행').first()
+    # print(vars(project))
+    # print(len(project.member_links))
+    # for member_link in project.member_links:
+    #     print(member_link.member.name)
 
 query(SessionLocal())
